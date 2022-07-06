faecb1fa992b9f5167583741d03c2d6377cf8ccd
Mariana West <marianahartenthal@gmail.com>
2022-07-06 20:04:10 +0100

    change to text instead of image, include maybe

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
-const img = document.querySelector(".algo-img");
-const algoAnswer = img;
-
+const result = document.querySelector("#result")
 
 document.querySelector('.algo-asks').addEventListener('click', event => {
     const userQuestion = document.getElementById("user-question").value;
-
     // to make the question section disappear on click
     document.querySelector('.question').classList.add('hidden');
-
     // to make sure the user enters a question
     if (userQuestion == "") {
-        img.src="img/question1024px.PNG";
-    } else {
+        result.innerText = "enter a yes-or-no question";
+    } 
+    else {
         let answer = Math.random();
-            if (answer < 0.33) {
-            img.src="img/YES1024px.png"
-            } else if (answer > 0.33 && answer < 0.66) {
-                img.src="img/NO1024px.png";               
+            if (answer < 0.25) {
+              result.innerText ="YES"
+            } else if (answer >= 0.25 && answer < 0.50) {
+                result.innerText="NO";               
+            } else if (answer >= 0.50 && answer < 0.75) {
+               result.innerText = "neither yes, nor no";
             } else {
-               img.src = "img/neither1024px.png";
-            };
-    }
-      });
+              result.innerText = "maybe";
+            };    
+    }});
 
       // to make question re-appear
-
       document.getElementById("result").addEventListener('click', event => {
         document.querySelector('.question').classList.remove('hidden');
-        img.src="img/black.PNG"
+        result.innerText = "";
+        // img.src="img/black.PNG"
       })
 
-
-
+     
